3c31af8362cf89c9ce91264e3fc761fa650e8227
muwongelawrence-tech <muwongelawrence44@gmail.com>
2024-01-09 17:51:06 +0300

    changes

--- service.py
+++ service.py
@@ -34,8 +34,11 @@
 
 import bentoml
 import torch
+import uuid
 from transformers import AutoTokenizer
-from typing import AsyncGenerator
+from typing import Any, AsyncGenerator, Dict, TypedDict, Union
+from bentoml import Service
+from bentoml.io import JSON, Text
 
 translation_runner = bentoml.models.get("text2textgeneration:latest").to_runner()
 
@@ -43,20 +46,40 @@ svc = bentoml.Service(
     name="englishtoluganda", runners=[translation_runner]
 )
 
-@svc.api(input=bentoml.io.Text(), output=bentoml.io.Text(content_type="text/event-stream"))
-async def translate(text: str, stream: bool = False) -> str:
-    # Assuming 'models' attribute is a list
-    # model_instance = translation_runner.models[0]
+# class GenerateInput(TypedDict):
+#     prompt: str
+#     stream: bool
+#     sampling_params: Dict[str, Any]
+
+GenerateInput = dict[str, Union[str, bool, dict[str, Any]]]
+
+
+@svc.api(
+    route="/translate",
+    input=JSON.from_sample(
+        GenerateInput(
+            prompt="What is fever?",
+            stream=False,
+            sampling_params={"temperature": 0.63, "logprobs": 1},
+        )
+    ),
+    output=Text(content_type="text/event-stream"),
+)
+
+async def translate(request: GenerateInput) -> Union[AsyncGenerator[str, None], str]:
+    n = request["sampling_params"].pop("n", 1)
+    request_id = f"englishtoluganda-{uuid.uuid4().hex}"
+    previous_texts = [[]] * n
 
     tokenizer = AutoTokenizer.from_pretrained('aceuganda/HEAL-BMG-grant-translation-english-luganda-v10')
-
     if tokenizer is None:
         return "Tokenizer not found in the model instance."
 
     # Tokenize the input text using the correct tokenizer
-    inputs = tokenizer(text, return_tensors='pt', padding=True)
+    inputs = tokenizer(request['prompt'], return_tensors='pt', padding=True)
 
-    # Assuming 'generate' is a method of the model instance for text generation
+    temperature = request["sampling_params"]["temperature"]
+
     generated = await translation_runner.async_run(**inputs)
 
     # Process the logits to obtain the text output
@@ -64,16 +87,21 @@ async def translate(text: str, stream: bool = False) -> str:
 
     # Use the same tokenizer for Decoding the output...
     output_text = tokenizer.batch_decode([output_ids], skip_special_tokens=True)[0]
-    
-    return output_text
-    # if stream:
-    #     async for line in _stream_output(output_text):
-    #         yield line
-    # else:
-    #     return output_text
 
-async def _stream_output(output_text: str) -> AsyncGenerator[str, None]:
-    # Split the text into lines and yield each line asynchronously
-    lines = output_text.split('\n')
-    for line in lines:
-        yield line
+
+    async def streamer() -> AsyncGenerator[str, None]:
+        async for request_output in output_text:
+            for output in request_output.outputs:
+                i = output.index
+                previous_texts[i].append(output.text)
+                yield output.text
+
+    if request["stream"]:
+        return streamer()
+    else:
+        return output_text
+        
+
+    async for _ in streamer():
+        pass
+    return "".join(previous_texts[0])
